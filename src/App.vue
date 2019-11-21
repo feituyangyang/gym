9c62e804a6ce32799b05219d56206cdcaa2714ba
zhouling <2528681598.qq.com>
2019-11-21 16:48:50 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <section class="App">
+    
+  </section>
+</template>
+
+<script>
+export default {
+ name: 'App',
+  data() {
+    return {
+
+    }
+  },
+  components: {},
+  watch: {},
+  computed: {},
+  mounted() {},
+  methods: {}
+}
+</script>
+
+<style scoped>
+</style>
